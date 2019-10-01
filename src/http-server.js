dd2a1acd46ae7236c4acc5e9c7104e8313815085
Drew <drw.spencer@gmail.com>
2019-10-01 17:49:32 -0500

    Authentication implemented

--- src/http-server.js
+++ src/http-server.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+const auth = require('./auth')
 var bodyParser = require('body-parser')
-var slideTracker = require('./slide-tracking/slide-tracker')
-var slideTrackerReports = require('./slide-tracking/reports')
 var slideTrackerCaseBlockSlideCounts = require('./slide-tracking/CaseBlockSlideCount.js')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+/*
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+}) */
+
+app.use(auth)
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -17,55 +27,7 @@ app.use(function (req, res, next) {
   next()
 })
 
-app.post('/getuserinfo', function (request, response) {
-  slideTracker.getUserInfo(request, response, function (err, message) {
-    if (err) return console.log(err)
-    console.log(message)
-  })
-})
-
-app.post('/getpartblockcurrentandtotals', function (request, response) {
-  slideTracker.getPartBlockCurrentAndTotals(request, response, function (err, message) {
-    if (err) return console.log(err)
-    console.log(message)
-  })
-})
-
-app.post('/updateslidetoprint', function (request, response) {
-  slideTracker.updateSlideToPrint(request, response, function (err, message) {
-    if (err) return console.log(err)
-    console.log(message)
-  })
-})
-
-app.post('/printslides', function (request, response) {
-  slideTracker.printSlides(request, response, function (err, message) {
-    if (err) return console.log(err)
-    console.log(message)
-  })
-})
-
-app.post('/histodata', function (request, response) {
-  slideTracker.histodata(request, response, function (err, message) {
-    if (err) return console.log(err)
-    console.log(message)
-  })
-})
-
-app.post('/slidedistribution', function (request, response) {
-  slideTracker.slideDistribution(request, response, function (err, message) {
-    if (err) return console.log(err)
-    console.log(message)
-  })
-})
-
-app.post('/reports', function (request, response) {
-  slideTrackerReports.reports(request, response, function (err, message) {
-    if (err) return console.log(err)
-    console.log(message)
-  })
-})
-
+/*
 // all routes prefixed with /slidetracker
 app.use('/slidetracker', router)
 console.log('Slide Tracker Default Route')
@@ -75,18 +37,13 @@ router.get('/', (request, response) => {
   console.log('router.get')
   response.json({ message: 'Hello from the API' })
 })
-
-router.get('/slideparameters', (request, response) => {
-  slideTracker.pullSlides(request, response, function (err, message) {
-    if (err) return console.log(err)
-    console.log(message)
-  })
-})
-
+*/
 app.post('/caseblockslidecount', (request, response) => {
   slideTrackerCaseBlockSlideCounts.caseblockslidecount(request, response, function (err, message) {
     if (err) return console.log(err)
+    console.log(request)
     console.log(message)
+    
   })
 })
 

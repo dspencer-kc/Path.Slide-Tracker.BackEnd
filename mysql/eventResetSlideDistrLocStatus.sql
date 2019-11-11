11b455b6597ce018b3391191b30805c6fc6cfc2f
Drew <drw.spencer@gmail.com>
2019-11-11 08:19:34 -0600

    Added event

---
+++ mysql/eventResetSlideDistrLocStatus.sql
@@ -0,0 +1,9 @@
+CREATE EVENT event_name
+  ON SCHEDULE
+    EVERY 1 DAY
+    STARTS (TIMESTAMP(CURRENT_DATE) + INTERVAL 1 DAY + INTERVAL 1 HOUR)
+  DO
+    UPDATE `OPENLIS`.`tblSlideDistributionLocations`
+       SET
+        `Status` = 'OPEN'
+       WHERE `Enabled` = 'Y';
